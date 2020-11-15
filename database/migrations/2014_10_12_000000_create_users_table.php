85bf443b902411ca0722a7dd6702a51bc2374c9d
eduardo sandon <eduardo.sandon@alumnos.ucn.cl>
2020-11-15 17:44:22 +0100

    prueba2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table-> Increments('id');
             $table->string('name');
+            $table->string('plastname');
+            $table->string('mlastname');
+            $table->string('rut_user');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('c_code');
             $table->rememberToken();
             $table->timestamps();
         });

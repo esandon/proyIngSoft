1572121c6d99236285d8bfff9969555cbaa15f51
eduardo sandon <eduardo.sandon@alumnos.ucn.cl>
2020-11-15 21:55:17 +0100

    hola

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Proyeco IngSoft
+                    Proyeco IngSofts
                 </div>
 
                 <div class="links">

f8ab0d7099b4fd08ddef612c3e6cb1be94bea853
Karina Ayabire <karina.ayabire@alumnos.ucn.cl>
2020-11-17 03:03:05 -0300

    Correcciones modelo Estudiante
    Se agregaron las variables al modelo estudiante

--- app/Estudiante.php
+++ app/Estudiante.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Estudiante extends Model
 {
-    //
+    protected $fillable = ['rut','apellido_paterno','apellido_materno','nombre','codigo_carrera','correo_electronico'];
 }

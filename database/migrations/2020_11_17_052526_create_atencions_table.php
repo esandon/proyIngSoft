03e7678397d56aa601b51448e33dfda255d2a359
Karina Ayabire <karina.ayabire@alumnos.ucn.cl>
2020-11-17 02:48:07 -0300

    Creación tabla Atención

---
+++ database/migrations/2020_11_17_052526_create_atencions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAtencionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('atencions', function (Blueprint $table) {
+            $table->date('fecha_publicacion');
+            $table->string('rut_estudiante');
+            $table->string('descripcion');
+            $table->string('medio_atencion');
+
+            $table->foreign('rut_estudiante')->references('rut')->on('estudiantes')->onDelete('cascade')->onUpdate('cascade');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('atencions');
+    }
+}

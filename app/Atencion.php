a9e273cb2d7b5b6a02e1b6e21c1c227b50ef0ff7
Karina Ayabire <karina.ayabire@alumnos.ucn.cl>
2020-11-17 03:02:25 -0300

    Correcciones en modelo atención
    Se agregaron las variables

--- app/Atencion.php
+++ app/Atencion.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Atencion extends Model
 {
-    //
+    protected $fillable = ['fecha_publicacion','rut_estudiante','descripcion','medio_atencion'];
 }

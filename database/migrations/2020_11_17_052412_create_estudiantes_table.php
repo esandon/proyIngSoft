47a1a0c9100a8254ecc2b29951fc80551d85eb14
Karina Ayabire <karina.ayabire@alumnos.ucn.cl>
2020-11-17 02:47:36 -0300

    Creación tabla estudiante
    Se creo la tabla Estudiante con sus respectivos atributos

---
+++ database/migrations/2020_11_17_052412_create_estudiantes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEstudiantesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estudiantes', function (Blueprint $table) {
+            $table->string('rut')->unique();
+            $table->string('apellido_paterno');
+            $table->string('apellido_materno');
+            $table->string('nombre');
+            $table->string('codigo_carrera');
+            $table->string('correo_electronico')->unique();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estudiantes');
+    }
+}

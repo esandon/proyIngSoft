b0657fa947457a2ba83441e3fcb208d74fa94da5
Karina Ayabire <karina.ayabire@alumnos.ucn.cl>
2020-11-17 03:21:11 -0300

    Correcciones modelos

--- app/Asignatura.php
+++ app/Asignatura.php
@@ -1,1 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Asignatura extends Model
+{
     protected $fillable = ['codigo','NRC','nombre'];
+    
+}
--- app/Atencion.php
+++ app/Atencion.php
@@ -1,1 +1,10 @@
-    protected $fillable = ['fecha_publicacion','rut_estudiante','descripcion','medio_atencion'];
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Atencion extends Model
+{
+    protected $fillable = ['fecha_publicacion','rut_estudiante','descripcion','medio_atencion','correo'];
+}

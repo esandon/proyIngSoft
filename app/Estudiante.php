454da055d717fadee4c07d2edb5fae1f55e6d6ab
Karina Ayabire <karina.ayabire@alumnos.ucn.cl>
2020-11-18 23:34:31 -0300

    .

--- app/Estudiante.php
+++ app/Estudiante.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Estudiante extends Model
 {
+
     protected $fillable = ['rut','apellido_paterno','apellido_materno','nombre','codigo_carrera','correo_electronico'];
 
     /**
